8799ef573705e4fb9410069bf1dbeb07e803a430
Joel Sherwin <sherwinjoel@gmail.com>
2019-11-13 00:20:20 +0530

    Get Podcast Title and Description from URL
    Get and display title and description of a podcast from an RSS feed URL
    
    TODO : Load cover art

--- app/src/main/java/com/example/orca_v01/MainActivity.java
+++ app/src/main/java/com/example/orca_v01/MainActivity.java
@@ -2,13 +2,217 @@ package com.example.orca_v01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText rssLink;
+    private Button fetchButtonVar;
+    private TextView name;
+    private TextView description;
+    private ImageView podImage;
+    private String podName;
+    private String podDesc;
+    private String imgUrl;
+    private List<RssFeedModel> mFeedModelList;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fetchButtonVar = findViewById(R.id.fetch);
+        rssLink = findViewById(R.id.rssLink);
+        name = findViewById(R.id.name);
+        description = findViewById(R.id.description);
+        podImage = findViewById(R.id.podcastImage);
+        fetchButtonVar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FetchFeedTask().execute((Void) null);
+            }
+        });
+    }
+
+    public List<RssFeedModel> parseFeed(InputStream inputStream) throws XmlPullParserException, IOException {
+        String title = null;
+        String link = null;
+        String description = null;
+        String url = null;
+        boolean isItem = false;
+        List<RssFeedModel> items = new ArrayList<>();
+
+        try {
+            XmlPullParser xmlPullParser = Xml.newPullParser();
+            xmlPullParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            xmlPullParser.setInput(inputStream, null);
+
+            xmlPullParser.nextTag();
+            while (xmlPullParser.next() != XmlPullParser.END_DOCUMENT) {
+                int eventType = xmlPullParser.getEventType();
+
+                String name = xmlPullParser.getName();
+                if(name == null)
+                    continue;
+
+                if(eventType == XmlPullParser.END_TAG) {
+                    if(name.equalsIgnoreCase("item")) {
+                        isItem = false;
+                    }
+                     continue;
+                }
+
+                if (eventType == XmlPullParser.START_TAG) {
+                    if(name.equalsIgnoreCase("item")) {
+                        isItem = true;
+                        continue;
+                    }
+                }
+
+                Log.d("MainActivity", "Parsing name ==> " + name);
+                String result = "";
+                if (xmlPullParser.next() == XmlPullParser.TEXT) {
+                    result = xmlPullParser.getText();
+                    xmlPullParser.nextTag();
+                }
+
+                if (name.equalsIgnoreCase("title")) {
+                    title = result;
+                } else if (name.equalsIgnoreCase("link")) {
+                    link = result;
+                } else if (name.equalsIgnoreCase("description")) {
+                    description = result;
+                }
+                else if (name.equalsIgnoreCase("url")) {
+                    Log.d("MainActivity", "URL " + result);
+                }
+
+                if (title != null && link != null && description != null) {
+                    if(isItem) {
+                        RssFeedModel item = new RssFeedModel(title, link, description);
+                        items.add(item);
+                    }
+                    else {
+                        podName = title;
+                        podDesc = description;
+                        imgUrl = url;
+                    }
+
+                    title = null;
+                    link = null;
+                    description = null;
+                    isItem = false;
+                }
+            }
+
+            return items;
+        } finally {
+            inputStream.close();
+        }
+    }
+
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src",src);
+
+
+            InputStream input = new java.net.URL(src).openStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
+    }
+
+
+
+    private class FetchFeedTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String urlLink;
+
+        @Override
+        protected void onPreExecute() {
+
+            podName = null;
+            podDesc = null;
+          //  name.setText("Feed Title");
+          //  description.setText("Feed Description: ");
+            urlLink = rssLink.getText().toString();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            if (TextUtils.isEmpty(urlLink))
+                return false;
+
+            try {
+                if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://"))
+                    urlLink = "http://" + urlLink;
+
+                URL url = new URL(urlLink);
+                InputStream inputStream = url.openConnection().getInputStream();
+                mFeedModelList = parseFeed(inputStream);
+                return true;
+            } catch (IOException e) {
+                Log.e(TAG, "Error", e);
+
+            } catch (XmlPullParserException e) {
+                Log.e(TAG, "Error", e);
+
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+
+
+            if (success) {
+                if(podName!=null) {
+                    name.setText(podName);
+                    description.setText(podDesc);
+                 //   podImage.setImageBitmap(getBitmapFromURL("https://megaphone-prod.s3.amazonaws.com/podcasts/05f71746-a825-11e5-aeb5-a7a572df575e/image/uploads_2F1569901799455-iwzhcytult-23e425914607b342a7b8aaaefebf8005_2FReplyAll_093019.jpg"));
+                    // Fill RecyclerView
+                }
+
+            } else {
+                Toast.makeText(MainActivity.this,
+                        "Enter a valid Rss feed url",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/orca_v01/RssFeedModel.java
@@ -0,0 +1,13 @@
+package com.example.orca_v01;
+
+public class RssFeedModel {
+    public String title;
+    public String link;
+    public String description;
+
+    public RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
